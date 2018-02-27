99bb78794600a54887b86171c1cc461a49507bdf
Ola <olamidelawal290@yahoo.co.uk>
2018-02-27 15:58:21 +0000

    Added employee class

--- app/src/main/java/uk/ac/mmu/employeedata/MainActivity.java
+++ app/src/main/java/uk/ac/mmu/employeedata/MainActivity.java
@@ -1,13 +1,28 @@
 package uk.ac.mmu.employeedata;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    //for making http call
+    HttpURLConnection urlConnection;
+
+    InputStream in = null;
+    String strURL = "http://10.0.2.2:8005";
+    String response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
